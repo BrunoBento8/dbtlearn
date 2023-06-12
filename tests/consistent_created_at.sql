3c7c9b5add3abd1723f58e380d07f6fce34ec426
Bruno Bento <99216093+BrunoBento8@users.noreply.github.com>
2023-06-12 17:00:19 -0300

    Documentation of the project

--- tests/consistent_created_at.sql
+++ tests/consistent_created_at.sql
@@ -1,6 +1,6 @@
 SELECT 
     * 
 FROM {{ ref('dim_listings_cleansed') }} l
-INNER JOIN {{ ref('fct_reviews') }} r
+INNER JOIN {{ ref('fct_review') }} r
 USING (listing_id)
 WHERE l.created_at >= r.review_date
